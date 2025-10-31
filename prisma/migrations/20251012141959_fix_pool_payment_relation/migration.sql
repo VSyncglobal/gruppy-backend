99dfc04b42616fb5d2d382eef2869fc0074da6c5
Samwel Chege <kingsamtex@gmail.com>
2025-10-31 21:36:16 +0300

    Force regenerate Prisma Client

--- prisma/migrations/20251012141959_fix_pool_payment_relation/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Pool" ADD COLUMN     "minJoiners" INTEGER NOT NULL DEFAULT 1;
--- prisma/migrations/20251015113141_make_payment_order_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."Payment" DROP CONSTRAINT "Payment_orderId_fkey";
-
--- AlterTable
-ALTER TABLE "Payment" ALTER COLUMN "orderId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Payment" ADD CONSTRAINT "Payment_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE SET NULL ON UPDATE CASCADE;
